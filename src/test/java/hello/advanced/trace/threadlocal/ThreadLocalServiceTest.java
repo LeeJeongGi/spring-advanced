1f609514b225bcc05399430dfb2af17adb87a3af
LeeJeongGi <apple4rhk@naver.com>
2022-05-16 22:23:13 +0900

    feat : ThreadLocal 사용하여 동시성 문제 해결 테스트

---
+++ src/test/java/hello/advanced/trace/threadlocal/ThreadLocalServiceTest.java
@@ -0,0 +1,47 @@
+package hello.advanced.trace.threadlocal;
+
+import hello.advanced.trace.threadlocal.code.ThreadLocalService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ThreadLocalServiceTest {
+
+    private ThreadLocalService service = new ThreadLocalService();
+
+    @Test
+    public void field() {
+        //given
+        log.info("main start");
+        Runnable userA = () -> {
+            service.login("userA");
+        };
+
+        Runnable userB = () -> {
+            service.login("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+        sleep(100); //동시성 문제 발생x
+        threadB.start();
+
+        sleep(3000); //메인 쓰레드 종료 대기
+        log.info("main exit");
+
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
